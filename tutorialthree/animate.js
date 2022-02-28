726df1f024a1564f0f66bb8b28f2cf2863acdbb4
krstnayou <95864385+krstnayou@users.noreply.github.com>
2022-02-28 15:22:45 -0500

    Add files via upload

---
+++ tutorialthree/animate.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+// alert("Created by Kristina You - 2022");
+ $("#title").draggable();
+ $("#rose").draggable();
+$("#endless").draggable();
+ $("#blond").draggable();
+  $("#nova").draggable();
+   $("#water").draggable();
+ $("#last").click(function(){
+$("#rose").show();
+
+});
+
+})
