26fd1659f76d75a00c037a8142d5840f21775e0b
krstnayou <95864385+krstnayou@users.noreply.github.com>
2022-03-28 07:52:20 -0400

    Add files via upload

---
+++ assignmentfive/animate.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+$("#bg").click(function(){
+  $("#title").fadeIn(3000);
+});
+
+$("#oneone").click(function(){
+  $("#onetwo").fadeIn(3000);
+});
+
+$("#onetwo").click(function(){
+  $("#onethree").fadeIn(3000);
+});
+
+$("#twoone").click(function(){
+  $("#twotwo").fadeIn(3000);
+});
+
+$("#twotwo").click(function(){
+  $("#twothree").fadeIn(3000);
+});
+
+$("#twothree").click(function(){
+  $("#twofour").fadeIn(3000);
+});
+
+$("#threeone").click(function(){
+  $("#threetwo").fadeIn(3000);
+});
+
+
+$("#threetwo").click(function(){
+  $("#threethree").fadeIn(3000);
+});
+
+$("#threethree").click(function(){
+  $("#threefour").fadeIn(3000);
+});
+
+$("#threefour").click(function(){
+  $("#threefive").fadeIn(3000);
+});
+
+$("#threefive").click(function(){
+  $("#threesix").fadeIn(3000);
+});
+
+});
+
+
